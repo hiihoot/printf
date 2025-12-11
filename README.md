8d7ea78a1f6f6744126f20284ebcc8ede40ae141
Salah Eddine Ait Mouhi <sait-mou@c1r4p7.1337.ma>
2025-12-11 11:28:22 +0100

    redone

---
+++ README.md
@@ -0,0 +1,20 @@
+#This project has been created as part of the 42 curriculum by sait-mou
+
+##Description
+reimplantation of the printf() function from libc, for the mandatory part I should implement the following conversions:
+
+• %c Prints a single character.
+• %s Prints a string (as defined by the common C convention).
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+• %% Prints a percent sign.
+
+##Instructions
+
+##Resources
+To learn about variadic functions:
+https://en.cppreference.com/w/c/variadic.html
--- ft_printf.c
+++ ft_printf.c
@@ -1,22 +1,61 @@
-#include "ft_printf.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-int	ft_printf(const char *format, ...)
+//• %c Prints a single character.
+int put_char(int c)
 {
-	va_list	ap;
-	int		count;
+    return (write(1, &c, 1));
+}
+//• %s Prints a string
+int put_str(const char *s)
+{
+    int count;
 
-	count = 0;
-	va_start (ap, format);
-	while (*(format) != '\0')
-	{
-		if (*format == '%' && *(format + 1) == '%')
-			count += write (1, format++, 1);
-		else if (*format == '%')
-			count += print_format (*(++format), ap);
-		else
-			count += write (1, format, 1);
-		format++;
-	}
-	va_end (ap);
-	return (count);
+    count = 0;
+    while (*s)
+    {
+        count += put_char(*s);
+        s++;
+    }
+    return (count);
+}
+//• %d Prints a decimal (base 10) number.
+
+
+//• %i Prints an integer in base 10
+
+int     ft_printf(const char* f, ...)
+{
+    int count;
+
+    va_list g;
+    va_start(g, f);
+
+    count = 0;
+    while (*f)
+    {
+        if (*f == '%')
+        {
+            f++;
+            if (*f == '%')
+                count += write(1, f, 1);
+            else if (*f == 'c')
+                count += put_char(va_arg(g, int));
+            else if (*f == 's')
+                count += put_str(va_arg(g, char *));
+            
+        }
+        f++;
+    }
+
+    printf("%d\n", count);
+
+    va_end(g);
+}
+
+int main()
+{
+    ft_printf("%% %c %s", 'd', "salah how are you?");
+    //printf("%% %s", "Salah how are you?");
 }
